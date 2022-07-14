6e7a80a6895f0a857779d242ffeb80d53ea4f4be
Jean-Emrick Rioux <gestionjerioux@gmail.com>
2022-07-14 17:21:48 -0400

    fix loading UI

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -4,35 +4,41 @@ import {useEffect, useState} from "react";
 
 const IsLoading = () => <div>Loading...</div>
 
-const Board = (props: any) => {
-    const {p, isLoading, total} = props;
-    console.log(total)
+const LoadedBoard = (props: any) => {
+    const {p, total} = props
     const formatter = Intl.DateTimeFormat('fr-CA', {
         dateStyle: "medium"
     })
+    return (
+        <>
+        <div className={"text-lg my-10"}>Les participants ont courus un total de <span className={"font-bold"}> {total._sum.distance} KM </span></div>
+    {p?.map((el: any) => {
+        return (
+            <div className="border-b border-t min-w-full" key={el.date.toISOString()}>
+                <div className="flex flex-row space-x-2 items-center py-2 px-6 justify-between">
+                    <div className={"flex flex-col"}>
+                        <div className={"text-xl"}>{el.name}</div>
+                        <div className={"text-md"}>{formatter.format(el.date)}</div>
+                    </div>
+                    <div className={"text-xl font-bold"}>{el.distance} KM</div>
+                </div>
+            </div>
+        )
+    })}
+        </>
+    )
+}
 
+const Board = (props: any) => {
+    const {p, isLoading, total} = props;
 
-    return isLoading? <IsLoading/> : (
+    return(
         <>
             <div className={"bg-white flex flex-col border-2 mx-auto mx-20 rounded-lg p-10 my-20"}>
                 <div className={"text-4xl"}>Résultats</div>
-                <div className={"text-lg my-10"}>Les participants ont courus un total de <span className={"font-bold"}> {total._sum.distance} KM </span></div>
-                {p?.map((el: any) => {
-                    return (
-                        <div className="border-b border-t min-w-full" key={el.date.toISOString()}>
-                            <div className="flex flex-row space-x-2 items-center py-2 px-6 justify-between">
-                                <div className={"flex flex-col"}>
-                                    <div className={"text-xl"}>{el.name}</div>
-                                    <div className={"text-md"}>{formatter.format(el.date)}</div>
-                                </div>
-                                <div className={"text-xl font-bold"}>{el.distance} KM</div>
-                            </div>
-                        </div>
-                    )
-                })}
+                {isLoading? <div>Loading...</div>: <LoadedBoard p={p} total={total}/>}
             </div>
 
-        </>
-    );
+        </>)
 };
 export default Board;
