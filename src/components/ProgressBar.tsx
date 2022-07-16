40bccfbc5fe5707464740c27ad320de68228b24d
Jean-Emrick Rioux <gestionjerioux@gmail.com>
2022-07-16 01:02:41 -0400

    fix progress bar responsive

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -8,18 +8,19 @@ const ProgressBar = (props: any) => {
     const formatter = Intl.NumberFormat("fr-CA", {
         style: "currency",
         currency: "CAD",
+        minimumFractionDigits: 0
     })
     const objectif = type === "currency" ? `${formatter.format(goal)}` : `${goal} KM`;
     const current = type === "currency" ? `${formatter.format(total)}` : `${total} KM`;
     console.log(percent)
     return(
         <>
-            <div className="h-10 md:h-12 relative max-w-4xl rounded-full overflow-hidden mb-20">
-                <div className="w-full h-full bg-primary absolute">
-                    <div className={"flex md:text-3xl text-xl text-white flex-row justify-end items-center pr-10 w-full h-full"}>{objectif}</div>
+            <div className="h-10 md:h-12 relative max-w-4xl rounded-full overflow-hidden mb-20 font-poppins">
+                <div className="w-full h-full bg-primary absolute ">
+                    <div className={"flex md:text-3xl text-sm text-white flex-row justify-end items-center md:pr-8 pr-4 w-full h-full"}>{objectif}</div>
                 </div>
                 <div style={ percent > 40 ? { width: `${percent}%`} : {width: "40%" }} className="h-full min-w-sm bg-white shadow-xl absolute rounded-full">
-                    <div className={"flex md:text-3xl text-xl flex-row justify-end items-center pr-10 w-full h-full text-dark"}>{current}</div>
+                    <div className={"flex md:text-3xl text-xl flex-row justify-end items-center md:pr-8 pr-4 w-full h-full text-dark"}>{current}</div>
                 </div>
             </div>
 
