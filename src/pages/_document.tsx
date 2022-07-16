3ad5de4f7e3f0c572eed8f6c7b124bb227777a9b
Jean-Emrick Rioux <gestionjerioux@gmail.com>
2022-07-15 23:32:34 -0400

    new font

---
+++ src/pages/_document.tsx
@@ -0,0 +1,27 @@
+import Document, {Html, Head, Main, NextScript, DocumentContext} from 'next/document'
+
+class MyDocument extends Document {
+    static async getInitialProps(ctx: DocumentContext) {
+        const initialProps = await Document.getInitialProps(ctx)
+        return { ...initialProps }
+    }
+
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <link
+                        href='https://fonts.googleapis.com/css2?family=Poppins:wght@400;800&family=Rubik&display=swap'
+                        rel="stylesheet"
+                    />
+                </Head>
+                <body className={"font-rubik"}>
+                <Main />
+                <NextScript />
+                </body>
+            </Html>
+        )
+    }
+}
+
+export default MyDocument
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,17 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        rubik: ["Rubik", "sans-serif"],
+      },
+      colors: {
+        primary: "#6D95AF",
+        secondary: "#B8D1DD",
+        dark: "#393838"
+      }
+    },
   },
   plugins: [],
 };
